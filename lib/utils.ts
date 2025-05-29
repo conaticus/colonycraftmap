a716f3410df47c7f79aef78db4e174f78c232f00
itsbrunodev <brunka215@gmail.com>
2025-05-29 22:46:09 +0200

    feat: add tileUrl util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function tileUrl(type: string) {
+  return decodeURI(
+    new URL(
+      `/tiles/${type}/{z}/{x}/{y}.png?v=${process.env.NEXT_PUBLIC_TILE_VERSION}`,
+      process.env.NEXT_PUBLIC_URL
+    ).href
+  );
+}
