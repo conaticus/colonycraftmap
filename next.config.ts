0f94596dddee2ac9917195506f16b74c41a12f71
itsbrunodev <brunka215@gmail.com>
2025-05-29 22:44:38 +0200

    chore: increment tiles version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "standalone",
   images: {
     formats: ["image/webp", "image/avif"],
     minimumCacheTTL: 31536000,
@@ -21,7 +20,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "ETag",
-            value: 'W/"tile-v1"',
+            value: 'W/"tiles-v2"',
           },
         ],
       },
@@ -44,36 +43,6 @@ const nextConfig: NextConfig = {
         ],
       },
     ];
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.usedExports = true;
-      config.optimization.sideEffects = false;
-
-      config.optimization.splitChunks = {
-        chunks: "all",
-        cacheGroups: {
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: "vendors",
-            priority: -10,
-            chunks: "all",
-          },
-          leaflet: {
-            test: /[\\/]node_modules[\\/](leaflet|react-leaflet)[\\/]/,
-            name: "leaflet",
-            priority: 10,
-            chunks: "all",
-          },
-        },
-      };
-    }
-    return config;
   },
   poweredByHeader: false,
 };
