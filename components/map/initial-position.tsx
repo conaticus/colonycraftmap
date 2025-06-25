483d7f16bb9f6ae37d5fce4a14de90fa8f3e8bbf
itsbrunodev <brunka215@gmail.com>
2025-06-25 20:45:49 +0200

    fix: temporarily use the local storage directly for getting and setting initial position

--- components/map/initial-position.tsx
+++ components/map/initial-position.tsx
@@ -1,18 +1,18 @@
 import { useMap } from "react-leaflet";
 import { useEffect } from "react";
 
-import { useLocalStorage } from "@/hooks/use-local-storage";
+// import { useLocalStorage } from "@/hooks/use-local-storage";
 
 export function InitialPositionHandler() {
   const map = useMap();
 
-  const [{ position, zoom }, setInitialPosition] = useLocalStorage<{
+  /* const [{ position, zoom }, setInitialPosition] = useLocalStorage<{
     position: L.LatLngTuple;
     zoom: number;
   }>("initial-position", {
     position: [0, 0],
     zoom: 3,
-  });
+  }); */
 
   // fly to initial position on load
   useEffect(() => {
@@ -20,18 +20,30 @@ export function InitialPositionHandler() {
       const center = map.getCenter();
       const zoom = map.getZoom();
 
-      setInitialPosition({
+      /* setInitialPosition({
         position: [center.lat, center.lng],
         zoom,
-      });
+      }); */
+
+      localStorage.setItem(
+        "initial-position",
+        JSON.stringify({ position: [center.lat, center.lng], zoom })
+      );
     }
 
     window.addEventListener("beforeunload", storeInitialPosition);
 
-    if (position[0] !== 0 && position[1] !== 0) {
-      map.flyTo(position, zoom, {
-        animate: false,
-      });
+    const data = localStorage.getItem("initial-position");
+
+    if (data) {
+      const position = JSON.parse(data).position as L.LatLngTuple;
+      const zoom = JSON.parse(data).zoom as number;
+
+      if (position[0] !== 0 && position[1] !== 0) {
+        map.flyTo(position, zoom, {
+          animate: false,
+        });
+      }
     }
 
     return () => {
@@ -41,9 +53,9 @@ export function InitialPositionHandler() {
     map.flyTo,
     map.getCenter,
     map.getZoom,
-    setInitialPosition,
+    /* setInitialPosition,
     position,
-    zoom,
+    zoom, */
   ]);
 
   return null;
