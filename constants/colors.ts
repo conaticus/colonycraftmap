f541f7539d6b8b0d017448e23c6b45598ff34cde
itsbrunodev <brunka215@gmail.com>
2025-07-14 23:59:12 +0200

    feat: add minecraft colors constant

---
+++ constants/colors.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+  black: "#000000",
+  dark_blue: "#0000AA",
+  dark_green: "#00AA00",
+  dark_aqua: "#00AAAA",
+  dark_red: "#AA0000",
+  dark_purple: "#AA00AA",
+  gold: "#FFAA00",
+  gray: "#AAAAAA",
+  dark_gray: "#555555",
+  blue: "#5555FF",
+  green: "#55FF55",
+  aqua: "#55FFFF",
+  red: "#FF5555",
+  light_purple: "#FF55FF",
+  yellow: "#FFFF55",
+  white: "#FFFFFF",
+};
